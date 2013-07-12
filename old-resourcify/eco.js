3d0727132ed45b9ec9d79514f496bdfb6f739197
Omar Khan <omar@omarkhan.me>
2013-07-12 14:16:57 +0100

    Remove old stuff

--- old-resourcify/eco.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(bundle){
-  bundle.register('.eco', function(data){
-    var eco = require('eco')
-    var content = eco.compile(data)
-    return "module.exports = " + content + ";"
-  })
-}
